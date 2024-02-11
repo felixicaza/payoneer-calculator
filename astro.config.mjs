ccfc6c72edfd8a5eba2d51ed5354ff8ac8ad7ee8
Felix Icaza <38303370+Felix-Icaza@users.noreply.github.com>
2024-02-11 14:20:46 -0600

    install and configure @vite-pwa/astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
+import AstroPWA from '@vite-pwa/astro'
 
 const websiteURL = 'https://payoneer-calculator.web.app/'
 
@@ -16,6 +17,160 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false
     }),
-    react()
+    react(),
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      start_url: '/?utm_source=web_app',
+      id: 'cal-pay',
+      includeAssets: ['/favicons/android-chrome-512x512.png'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Calculadora Payonner',
+        short_name: 'Calculadora Payoneer',
+        description:
+          'Calculadora de Impuestos de Payoneer, con la cual puedes calcular todas las comisiones aplicadas al momento de realizar un retiro de un cajero en Nicaragua.',
+        dir: 'ltr',
+        lang: 'es',
+        display: 'standalone',
+        orientation: 'portrait',
+        background_color: '#ffece5',
+        theme_color: '#ffece5',
+        icons: [
+          {
+            src: '/manifest/icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/manifest/maskable-icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/manifest/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/manifest/maskable-icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/manifest/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/manifest/maskable-icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/manifest/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/manifest/maskable-icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/manifest/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/manifest/maskable-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/manifest/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/manifest/maskable-icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/manifest/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/manifest/maskable-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/manifest/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/manifest/maskable-icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/manifest/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/manifest/maskable-icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/manifest/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/manifest/maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}']
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//]
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true
+      }
+    })
   ]
 })
