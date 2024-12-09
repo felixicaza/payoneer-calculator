2cd98f7d834c735d37842d088fac7c44cdc8f6e0
Felix Icaza <fx.joliett17@gmail.com>
2024-12-09 10:58:26 -0600

    chore: add reference to Astro types in env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="vite-plugin-pwa/info" />
 /// <reference types="vite-plugin-pwa/vanillajs" />
