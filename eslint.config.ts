23a9c9e5dee865883abe06d39f7f2ced07f1952d
Felix Icaza <fx.joliett17@gmail.com>
2025-04-19 16:28:59 -0600

    feat(lint): remove old ESLint configuration and add new TypeScript-based ESLint setup

---
+++ eslint.config.ts
@@ -0,0 +1,70 @@
+import type { Linter } from 'eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginReact from '@eslint-react/eslint-plugin'
+import { configs as configsAstro } from 'eslint-plugin-astro'
+import pluginJsonc from 'eslint-plugin-jsonc'
+import pluginPackageJson from 'eslint-plugin-package-json'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+import pluginReactRefresh from 'eslint-plugin-react-refresh'
+import pluginTailwind from 'eslint-plugin-tailwindcss'
+import pluginYml from 'eslint-plugin-yml'
+import neostandard, { plugins, resolveIgnoresFromGitignore } from 'neostandard'
+import parserTs from '@typescript-eslint/parser'
+
+console.log(resolveIgnoresFromGitignore())
+
+export default defineConfig([
+  globalIgnores(resolveIgnoresFromGitignore()),
+  { languageOptions: { globals: { ...globals.browser, ...globals.node, ...globals.serviceworker } } },
+  pluginJs.configs.recommended,
+  neostandard({
+    noJsx: true,
+    ts: true
+  }),
+  plugins.promise.configs['flat/recommended'],
+  plugins['@stylistic'].configs['recommended-flat'],
+  pluginJsonc.configs['flat/recommended-with-jsonc'],
+  pluginPackageJson.configs.recommended,
+  pluginYml.configs['flat/recommended'],
+  pluginTailwind.configs['flat/recommended'],
+  {
+    files: ['src/**/*.tsx'],
+    languageOptions: { parser: parserTs, parserOptions: { ecmaFeatures: { jsx: true } } },
+    ...pluginReact.configs.recommended,
+    ...pluginReactHooks.configs['recommended-latest'],
+    ...pluginReactRefresh.configs.recommended
+  },
+  configsAstro['flat/jsx-a11y-recommended'],
+  configsAstro['flat/recommended'],
+  {
+    files: ['src/**/*.astro'],
+    rules: {
+      '@stylistic/jsx-one-expression-per-line': ['error', { allow: 'non-jsx' }]
+    }
+  },
+  {
+    files: ['src/**/*.astro/*.ts'],
+    rules: {
+      '@stylistic/indent': ['error', 2]
+    }
+  },
+  {
+    rules: {
+      '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/quote-props': ['error', 'as-needed'],
+      '@stylistic/comma-dangle': ['error', 'never'],
+      '@stylistic/no-multi-spaces': ['error', { ignoreEOLComments: false }],
+
+      'tailwindcss/no-custom-classname': 'off',
+
+      'astro/jsx-a11y/anchor-is-valid': 'warn',
+
+      'yml/indent': ['error', 3, { indicatorValueIndent: 2 }],
+      'yml/quotes': ['error', { prefer: 'double' }]
+    }
+  }
+]) as Linter.Config
