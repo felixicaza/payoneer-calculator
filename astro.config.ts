9d1f654bb57ee456185b526cf605cc2acdfbf8b7
Felix Icaza <fx.joliett17@gmail.com>
2025-05-13 21:48:35 -0600

    feat: adds SEO, compression and integrates sitemap generation

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config'
 
 import react from '@astrojs/react'
+import sitemap from 'astro-sitemap'
+import playformCompress from '@playform/compress'
+import compressor from 'astro-compressor'
 import AstroPWA from '@vite-pwa/astro'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -13,6 +16,11 @@ export default defineConfig({
   server: {
     host: true
   },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport'
+  },
+  compressHTML: false,
   vite: {
     css: {
       transformer: 'lightningcss'
@@ -172,6 +180,69 @@ export default defineConfig({
       experimental: {
         directoryAndTrailingSlashHandler: true
       }
-    })
+    }),
+    sitemap({
+      canonicalURL: websiteURL,
+      lastmod: new Date(),
+      createLinkInHead: false,
+      xmlns: {
+        news: false,
+        video: false,
+        image: false,
+        xhtml: true
+      },
+      i18n: {
+        defaultLocale: 'es',
+        locales: {
+          es: 'es'
+        }
+      },
+      // Remove trailing slash
+      serialize(item) {
+        item.url = item.url.replace(/\/$/g, '')
+        return item
+      }
+    }),
+    playformCompress({
+      HTML: {
+        'html-minifier-terser': {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          maxLineLength: 0,
+          minifyCSS: true,
+          minifyJS: true,
+          minifyURLs: true,
+          noNewlinesBeforeTagClose: true,
+          removeAttributeQuotes: false,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          sortAttributes: true,
+          sortClassName: true,
+          useShortDoctype: true
+        }
+      },
+      JavaScript: {
+        terser: {
+          compress: {
+            arguments: true,
+            drop_console: true
+          },
+          format: {
+            comments: false,
+            indent_level: 2
+          },
+          ecma: 2020
+        }
+      },
+      Cache: true,
+      CSS: false,
+      Image: false,
+      SVG: false
+    }),
+    compressor()
   ]
 })
