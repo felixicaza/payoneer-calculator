0733e8115d683ff1555249c90d5bb16d48f83992
Felix Icaza <fx.joliett17@gmail.com>
2025-05-14 18:19:33 -0600

    feat: add constants and update tsconfig path

---
+++ src/data/constants.ts
@@ -0,0 +1,6 @@
+export const URL = 'https://payoneer-calculator.web.app/'
+export const TITLE = 'Calculadora Payoneer'
+export const DESCRIPTION = 'Calcula de manera FÁCIL, RÁPIDA Y SEGURA, todas las COMISIONES DE PAYONEER Y DEL BANCO aplicadas al momento de realizar un retiro de un CAJERO ATM en Nicaragua.'
+export const COLOR = '#ffece5'
+
+export const ICONS_SIZES = [48, 72, 96, 128, 144, 152, 192, 256, 384, 512]
